f50db3cb62b2e4603bd409453e5e51f5c5cd99eb
Erik Oehlke <eoehlke@gmail.com>
2022-02-14 11:15:27 -0600

    Committing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
 // import logo from './logo.svg';
 import logo from './images/DesignGif.gif';
 import Typewriter from 'typewriter-effect';
+import { Parallax, useParallax } from 'react-scroll-parallax';
 import FadeIn from 'react-fade-in/lib/FadeIn';
 
 import './App.css';
-import { selectOptions } from '@testing-library/user-event/dist/select-options';
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="Text-block">
-          <h3 className='typeWriter'><Typewriter onInit={(typewriter) => {
-            typewriter.typeString("Hi, I'm Erik Oehlke.")
-            typewriter.typeString("<br/>Welcome to my Portfolio")
-              .changeDelay('natural')
-              .start();
-          }}
-          />
-          </h3>
-          <FadeIn delay="7500"><h4 className='topics'>
-            About Me<br/>
-            Projects<br/>
-            Hobbies
-          </h4>
-          </FadeIn>
-        </div>
-        
+      <img src={logo} className="App-logo" alt="logo" />
+      <h4 className='topics'>
+        <h4 className='Projects'>Projects</h4>
+        <h4 className='About'>About Me</h4>
+        <h4 className='Hobbies'>Hobbies</h4>
+      </h4>
+      <div className="Text-block">
+        <h3 className='typeWriter'><Typewriter onInit={(typewriter) => {
+          typewriter.typeString("Hi, I'm Erik Oehlke.")
+            // typewriter.typeString("<br/>Welcome to my Portfolio")
+            .start();
+        }}
+        />
+        </h3>
       </div>
+        <div className='Para'>
+        </div>
+          <div className='Pro'>
+          </div>
+ 
     </div>
   );
 }
